b157da6cf0da5a73df5a17522da635c8409708f0
David Marcillo <davo0712@hotmail.com>
2019-03-08 18:15:43 -0500

    Add files via upload

---
+++ PruebaVue/01.js
@@ -0,0 +1,12 @@
+const tittle = new Vue({
+  el:'#tittle',
+  data:{
+    titulo:'BIENVENIDO TEST VUE',
+    nombres:[
+      {integrante:'David',apellido:'Marcillo'},
+      {integrante:'Freddy',apellido:'Lopez'},
+      {integrante:'Carolina',apellido:'Calvopiña'},
+      {integrante:'Wilma',apellido:'Quinatoa'}
+    ]
+  }
+})
